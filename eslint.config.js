ccaa7328b943dacc580fd8bed10db2428bb38ab1
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2026-03-10 06:20:17 +0200

    exercise 10.2: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+const js = require("@eslint/js");
+const babelParser = require("@babel/eslint-parser");
+const reactPlugin = require("eslint-plugin-react");
+const reactNativePlugin = require("eslint-plugin-react-native");
+
+const reactNativeGlobals =
+  reactNativePlugin.environments?.["react-native"]?.globals || {};
+
+module.exports = [
+  js.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  {
+    plugins: {
+      react: reactPlugin,
+      "react-native": reactNativePlugin,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      parser: babelParser,
+      globals: reactNativeGlobals,
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
