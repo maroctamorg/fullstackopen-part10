fb3cd51123135903dc4a7f1e57c253441aad8afb
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2026-03-11 20:19:06 +0200

    resolve merge conflicts in submodule

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,24 +1,13 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client/react";
+
+import { GET_REPOSITORIES } from "../graphql/queries/repositories";
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true);
-
-    const response = await fetch("http://192.168.1.165:5000/api/repositories");
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories: data?.repositories, loading };
 };
 
 export default useRepositories;
